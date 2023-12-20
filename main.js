31ee84b3cad6fa92fed91f565b395e6221f8ad4d
8490 <ethan@clarusway.com>
2023-12-20 16:11:44 +0300

    add get coins url

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ import "./scss/style.scss";
 const form = document.querySelector("header form");
 
 form.addEventListener("submit", (e) => {
-  console.log(e.target);
+  //   console.log(e.target);
   e.preventDefault();
   getCoinData();
+  e.target.reset();
 });
 
 const getCoinData = async () => {
   const input = document.querySelector("header form input").value;
-  console.log(input);
+  //   console.log(input);
   const API_KEY = "coinranking5ee965ec6e62498f585603ededb1b9ea25b9ac0bae84330a";
-  const options = { headers: { "x-access-token": "API_KEY" } };
+  const options = { headers: { "x-access-token": API_KEY } };
 
-  const url = `https://api.coinranking.com/v2/coins?search={input}`;
-  //   const res = await fetch();
-  console.log(url);
+  const url = `https://api.coinranking.com/v2/coins?search=${input}`;
+  const res = await fetch(url, options);
+  const json = await res.json();
+  console.log(json.data.coins);
 };
