181f9c6056cbec6ca37bf1046019572339a486f9
8490 <ethan@clarusway.com>
2024-01-02 13:02:52 +0300

    add close icon

--- src/renderData.js
+++ src/renderData.js
@@ -1,4 +1,14 @@
 export const renderCoins = (coin) => {
   console.log(coin);
-  const { price, name, iconUrl, change, symbol } = coin;
+  const { price, name, iconUrl, change, symbol, rank } = coin;
+
+  const coinsUl = document.querySelector("main .coins");
+
+  const newCoinLi = document.createElement("li");
+  newCoinLi.innerHTML = `
+  <div class="remove-icon">
+     <i class="fas fa-window-close"> </i>
+  </div>
+  `;
+  coinsUl.append(newCoinLi);
 };
